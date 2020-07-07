a1a9ddab91ea589cb2f5ee4520abb4035ef2a39e
lee-shun <lee970802@163.com>
2020-07-07 15:04:56 +0800

    update

--- autostart.sh
+++ autostart.sh
@@ -5,6 +5,6 @@ picom -o 0.95 -i 0.88 --detect-rounded-corners --vsync --blur-background-fixed -
 feh --bg-scale ~/图片/wallhaven-vg6dqp.jpg &
 nm-applet &
 xfce4-power-manager &
-/bin/bash ./show_status_bar.sh
+/bin/bash ./show_status_bar.sh &
 /bin/bash ./tap-to-click.sh
 
