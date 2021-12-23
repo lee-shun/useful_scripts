5ed8e7d568f10895d9629d4ee75a935a524f7c45
lee-shun <lee970802@163.com>
2021-12-23 09:36:27 -0500

    U

---
+++ change_background.sh
@@ -0,0 +1,24 @@
+#!/system/bin/sh
+
+
+# ------------------------------------------------------------------------------
+#
+#   Copyright (C) 2021 Concordia NAVlab. All rights reserved.
+#
+#   @Filename: change_background.sh
+#
+#   @Author: Shun Li
+#
+#   @Date: 2021-12-23
+#
+#   @Email: 2015097272@qq.com
+#
+#   @Description: 
+#
+# ------------------------------------------------------------------------------
+
+while true;
+do
+    feh --gb-fill --no-fehbg --randomize /home/ls/图片
+    sleep 15
+done
