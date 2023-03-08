b7463f5727d6e0f9cd3241b6a3acde0ce3442dfb
ShawnLee <lee970802@163.com>
2023-03-07 20:31:52 -0500

    u

---
+++ lateral.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output eDP-1-1 --primary --mode 2880x1800 --pos 0x0 --rotate normal --output HDMI-1-1 --mode 1920x1080 --pos 2880x0 --scale 1.5x1.5 --rotate normal --output DP-1-1 --off --output DP-1-2 --off --output DP-1-3 --off --output DP-1-4 --off
